367759beacfe63ffee0c7ffc258a8913caeb9cd6
mesoma-hub <anigbogumesoma1@gmail.com>
2022-07-19 03:56:47 -0700

    quick sort hoare fix

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,62 +1,72 @@
 #include "sort.h"
-
 /**
- * _swap - swaps two values in an array
- *
- * @array: data to sort
- * @i: first value
- * @j: second value
- *
- * Return: No Return
- */
-void _swap(int *array, int i, int j)
+*swap - the positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, ssize_t item1, ssize_t item2)
 {
 	int tmp;
 
-	tmp = array[i];
-	array[i] = array[j];
-	array[j] = tmp;
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
 }
-
 /**
- * partition - sorts a partition of data in relation to a pivot
- *
- * @array: data to sort
- * @min: Left wall
- * @max: right wall
- * @size: size of data
- *
- * Return: New Pivot
+ *hoare_partition - hoare partition sorting scheme implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: size array
+ *Return: return the position of the last element sorted
  */
-int partition(int *array, int min, int max, size_t size)
+int hoare_partition(int *array, int first, int last, int size)
 {
-	int i, j, pivot = array[max];
+	int current = first - 1, finder = last + 1;
+	int pivot = array[last];
 
-	for (i = min, j = max; 1; i++, j--)
+	while (1)
 	{
-		while (array[i] < pivot)
-			i++;
 
-		while (array[j] > pivot)
-			j--;
-
-		if (i >= j)
-			return (i);
-		_swap(array, i, j);
+		do {
+			current++;
+		} while (array[current] < pivot);
+		do {
+			finder--;
+		} while (array[finder] > pivot);
+		if (current >= finder)
+			return (current);
+		swap(array, current, finder);
 		print_array(array, size);
 	}
 }
-
 /**
- * quicksort -  sorts an array of integers in ascending order using the
- * Quick sort algorithm Lomuto partition scheme
- *
- * @array: data to sort
- * @min: Left wall
- * @max: right wall
- * @size: size of data
- *
- * Return: No Return
+ *qs - qucksort algorithm implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: array size
  */
-void quicksort(int *array, int min, int max, size_t size)
+void qs(int *array, ssize_t first, ssize_t last, int size)
+{
+	ssize_t position = 0;
 
+	if (first < last)
+	{
+		position = hoare_partition(array, first, last, size);
+		qs(array, first, position - 1, size);
+		qs(array, position, last, size);
+	}
+}
+/**
+ *quick_sort_hoare - prepare the terrain to quicksort algorithm
+ *@array: array
+ *@size: array size
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	qs(array, 0, size - 1, size);
+}
